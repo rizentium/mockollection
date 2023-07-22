ee9a0ff4134947a8335343e4419c7ebc5bac4648
Arif Hidayat <rizentium@gmail.com>
2023-07-23 00:12:35 +0700

    feat(home): update screen design

---
+++ lib/presentation/home/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+
+  Widget create() {
+    return HomeScreen(title: title);
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Mockollection"),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              "Mock Collection",
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16.0),
+            FilledButton(
+              onPressed: () {},
+              child: const Text('Instagram'),
+            ),
+            const SizedBox(height: 16.0),
+            FilledButton(
+              onPressed: () {},
+              child: const Text('Twitter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
